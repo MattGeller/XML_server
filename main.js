b75e2f0bdf07e9b83cc9824e6b7c9d7f716e530c
Matt Geller <MattG92@gmail.com>
2017-10-21 18:24:40 -0700

    in progress

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
 $(document).ready(function(){
+    console.log('document ready');
+
     $.ajax({
         type: 'GET',
         url: 'main.php',
@@ -7,8 +9,21 @@ $(document).ready(function(){
             console.log('data seems good',resp);
             //take the table out of the resp object and add it to the DOM
         },
-        failure: function(resp){
+        error: function(resp){
             console.log('something went wrong retrieving data', resp);
         }
     });
+
+    // $.ajax({
+    //     type: 'GET',
+    //     url: 'main.php',
+    //     dataType: 'json',
+    //     success: function(resp){
+    //         console.log('data seems good',resp);
+    //         //take the table out of the resp object and add it to the DOM
+    //     },
+    //     error: function(resp){
+    //         console.log('something went wrong retrieving data', resp);
+    //     }
+    // });
 });
--- main.php
+++ main.php
@@ -1,10 +1,31 @@
 <?php
+header('Content-Type: text/xml');
+
 //this php script is executed based on a request from the client
 
 //make a cURL request to the remote server
 
 //turn data from JSON into XML (just to simulate if the data came back as XML in the first place)
+//JSON decode into an associative array
+//make into XML string
+//use file_put_contents() to save XML string
 
-//save XML response as a file in a separate folder
+//make XML string into SimpleXMLElement
+
 
 //make a bootstrap table in an XML object to send back to the client
+
+//send the XML back to the client
+
+
+//$output = [
+//    'someKey' => 'someKeyValue',
+//    'someOtherKey' => 'someOtherKeyValue',
+//    'someArray' => [5,4,3,2,1]
+//];
+
+$output = "<div><success>true</success><div>Hi there!</div></div>";
+
+$output = new SimpleXMLElement($output);
+
+print($output->asXML());
