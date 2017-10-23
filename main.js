da09767a01b6ed49e38280a1751842d9d03df1f3
Matt Geller <MattG92@gmail.com>
2017-10-22 17:19:30 -0700

    learning how to use XML

--- main.js
+++ main.js
@@ -4,10 +4,25 @@ $(document).ready(function(){
     $.ajax({
         type: 'GET',
         url: 'main.php',
-        dataType: 'json',
+        dataType: 'xml',
         success: function(resp){
             console.log('data seems good',resp);
             //take the table out of the resp object and add it to the DOM
+
+            // //before an XML document can be accessed, it must be loaded into an XML DOM object
+            // const parser = new DOMParser(); //get our parser object ready to use
+            // const changedResp = parser.parseFromString(resp, "text/xml"); //resp is now
+            // console.log('changed resp:', changedResp);
+
+            //use jQuery to look through the xml response
+            $(resp).find('entry').each(function(){
+                document.body.innerHTML += $(this).find('title').text();
+            });
+
+            //actually, just use jQuery to find the table already made for the front end and add it straight to the dom
+
+
+            // document.body.innerHTML += resp.attributes;
         },
         error: function(resp){
             console.log('something went wrong retrieving data', resp);
--- main.php
+++ main.php
@@ -9,7 +9,7 @@ header('Content-Type: text/xml');
 $ch = curl_init();
 
 //set the url
-curl_setopt($ch,CURLOPT_URL, 'https://api.pokemontcg.io/v1/cards?pageSize=3');
+curl_setopt($ch,CURLOPT_URL, 'http://ax.itunes.apple.com/WebObjects/MZStoreServices.woa/ws/RSS/topMovies/xml');
 
 // Tell it to return the response, if false it print the response
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
@@ -17,18 +17,13 @@ curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 //execute
 $resp = curl_exec($ch);
 
-//TODO: save the $resp into a file so I can see what's going on
-file_put_contents();
-
-print_r($resp);
-
-//turn data from JSON into XML (just to simulate if the data came back as XML in the first place)
-//JSON decode into an associative array
-//make into XML string
-//use file_put_contents() to save XML string
+//use file_put_contents() to save XML string to a file
+file_put_contents('target.txt', $resp);
 
 //make XML string into SimpleXMLElement
+$resp = new SimpleXMLElement($resp);
 
+//use xpath to parse through the response
 
 //make a bootstrap table in an XML object to send back to the client
 
@@ -41,8 +36,12 @@ print_r($resp);
 //    'someArray' => [5,4,3,2,1]
 //];
 
-$output = "<div><success>true</success><div>Hi there!</div></div>";
+//$output = "<div><success>true</success><div>Hi there!</div></div>";
 
-$output = new SimpleXMLElement($output);
+//$output = new SimpleXMLElement($output);
 
 //print($output->asXML());
+
+//print($resp)
+
+print($resp->asXML());
